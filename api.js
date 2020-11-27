106e0627ba7a8d5016c0db560772b5773ad3b895
Delucse <46593742+Delucse@users.noreply.github.com>
2020-11-27 17:18:34 +0100

    initialize mongoDB

--- api.js
+++ api.js
@@ -1,41 +1,51 @@
+// jshint esversion: 8
+// jshint node: true
+"use strict";
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var api = express();
 
-var app = express();
+//reads in configuration from a .env file
+require('dotenv').config();
+
+api.use(cors());
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+api.set('views', path.join(__dirname, 'views'));
+api.set('view engine', 'jade');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+api.use(logger('dev'));
+api.use(express.json());
+api.use(express.urlencoded({ extended: false }));
+api.use(cookieParser());
+api.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+var tutorialRouter = require('./routes/tutorial/index');
+var usersRouter = require('./routes/users');
+
+api.use('/tutorial', tutorialRouter);
+api.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+api.use(function(req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+api.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.send(err);
 });
 
-module.exports = app;
+module.exports = api;
---
+++ helper/mongoConnection.js
@@ -0,0 +1,29 @@
+// jshint esversion: 8
+// jshint node: true
+"use strict";
+
+const mongoose = require('mongoose');
+const chalk = require('chalk');
+
+const mongoUsername = process.env.MONGO_USERNAME;
+const mongoPassword = process.env.MONGO_PASSWORD;
+
+const connectMongoDB = async function(cb) {
+  await mongoose.connect(`mongodb://${mongoUsername}:${mongoPassword}@localhost/blockly`, {
+    authSource: 'test',
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    autoReconnect: true
+  }).then(db => {
+    console.log(chalk.green('Connected to MongoDB (databasename: "'+db.connections[0].name+'") on host "'+db.connections[0].host+'" and on port "'+db.connections[0].port+'""'));
+    cb();
+  }).catch(err => {
+    console.log(chalk.red('Error at MongoDB-connection with Localhost: '+err));
+    console.log(chalk.red('Retry to connect in 3 seconds'));
+    setTimeout(connectMongoDB, 3000, cb); // retry until db-server is up
+  });
+};
+
+module.exports = {
+  connectMongoDB
+};
