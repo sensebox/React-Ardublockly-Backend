4da6bbda5fb8d72e101f89eb84260b3c4b890a2e
Delucse <46593742+Delucse@users.noreply.github.com>
2020-12-07 11:33:37 +0100

    details about boxes are available after login

--- routes/user/user/me.js
+++ routes/user/user/me.js
@@ -11,13 +11,34 @@ const User = require('../../../models/user');
 const me = async function(req, res){
   try{
     var body = req.user;
-    var user = await User.findOne({email: body.email});
-    body.blocklyRole = user.role;
-    body.badge = user.badge;
-    return res.status(200).send({
-      message: 'User found successfully.',
-      user: body
-    });
+    request.get('https://api.opensensemap.org/users/me/boxes', {
+      headers: {
+        'Content-type': 'application/json',
+        'Authorization': req.header('authorization')
+      }})
+      .on('response', function(response) {
+        // concatenate updates from datastream
+        var boxesBody = '';
+        response.on('data', function(chunk){
+          boxesBody += chunk;
+        });
+        response.on('end', async function(){
+          if(response.statusCode !== 200){
+            return res.status(401).send({
+              message: 'Unauthorized',
+            });
+          }
+          boxesBody = JSON.parse(boxesBody);
+          var user = await User.findOne({email: body.email});
+          body.blocklyRole = user.role;
+          body.badge = user.badge;
+          body.boxes = boxesBody.data.boxes;
+          return res.status(200).send({
+            message: 'User found successfully.',
+            user: body
+          });
+        });
+      });
   }
   catch(err){
     return res.status(500).send(err);
