d95d5ab748cd4adce402e457a3ab59ff1765dd71
Delucse <46593742+Delucse@users.noreply.github.com>
2020-12-07 11:33:37 +0100

    details about boxes are available after login

--- routes/user/user/login.js
+++ routes/user/user/login.js
@@ -29,20 +29,46 @@ const login = function(req, res){
               message: 'User and or password not valid.',
             });
           }
-          // check if user already exists in blockly-user-db
           body = JSON.parse(body);
-          var user = await User.findOneAndUpdate({email: body.data.user.email}, // query
-                                            {$setOnInsert: {email: body.data.user.email}}, // update if not exist
-                                            {upsert: true}); // options
-          body.data.user.blocklyRole = user.role;
-          body.data.user.badge = user.badge;
-          return res.status(200).send({
-            message: 'Successfully signed in.',
-            user: body.data.user,
-            token: body.token,
-            refreshToken: body.refreshToken
+          // get all information about all boxes from signed in user
+          request.get('https://api.opensensemap.org/users/me/boxes', {
+            headers: {
+              'Content-type': 'application/json',
+              'Authorization': `Bearer ${body.token}`
+            }})
+            .on('response', function(response) {
+              // concatenate updates from datastream
+              var boxesBody = '';
+              response.on('data', function(chunk){
+                boxesBody += chunk;
+              });
+              response.on('end', async function(){
+                if(response.statusCode !== 200){
+                  return res.status(403).send({
+                    message: 'User and or password not valid.',
+                  });
+                }
+                boxesBody = JSON.parse(boxesBody);
+                body.data.user.boxes = boxesBody.data.boxes;
+                // check if user already exists in blockly-user-db
+                var user = await User.findOneAndUpdate({email: body.data.user.email}, // query
+                                                  {$setOnInsert: {email: body.data.user.email}}, // update if not exist
+                                                  {upsert: true}); // options
+                body.data.user.blocklyRole = user.role;
+                body.data.user.badge = user.badge;
+                console.log(2, body.data.user.boxes);
+                return res.status(200).send({
+                  message: 'Successfully signed in.',
+                  user: body.data.user,
+                  token: body.token,
+                  refreshToken: body.refreshToken
+                });
+              });
+            });
+          })
+          .on('error', function(err) {
+            return res.status(500).send(err);
           });
-        });
       })
       .on('error', function(err) {
         return res.status(500).send(err);
