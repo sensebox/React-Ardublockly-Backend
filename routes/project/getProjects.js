1b811ba81613d89eba0231b6f0dd5d87d178511f
Delucse <46593742+Delucse@users.noreply.github.com>
2020-12-08 20:18:01 +0100

    add property 'shared' to project

--- routes/project/getProjects.js
+++ routes/project/getProjects.js
@@ -9,23 +9,26 @@ const Project = require('../../models/project');
 const Share = require('../../models/share');
 
 const getProjects = async function(req, res){
-  // try{
+  try{
     var result = await Project.find({creator: req.user.email});
-    var populateResult = await Project.find({creator: req.user.email}).populate({path: '_id', select: 'expiresAt'});
+    var populateResult = await Project.find({creator: req.user.email}).lean().populate({path: '_id', select: 'expiresAt'});
+    // check if project has already been shared and add expire date if necessary
     var projects = populateResult.map((res, i) => {
-      if(!res._id){
-        res._id = result[i]._id;
+      res.shared = null;
+      if(res._id && res._id._id){
+        res.shared = res._id.expiresAt;
       }
+      res._id = result[i]._id;
       return res;
     });
     return res.status(200).send({
       message: 'Projects found successfully.',
       projects: projects
     });
-  // }
-  // catch(err){
-  //   return res.status(500).send(err);
-  // }
+  }
+  catch(err){
+    return res.status(500).send(err);
+  }
 };
 
 module.exports = {
