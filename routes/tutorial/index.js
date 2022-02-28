a5d05765c924f4a799e0230aeb774cdf02ed0d84
Mario Pesch <mario.pesch@uni-muenster.de>
2022-02-28 19:58:09 +0100

    add public and review to model
    update tutorial model
    - add public and review to tutorial model

--- routes/tutorial/index.js
+++ routes/tutorial/index.js
@@ -2,25 +2,41 @@
 // jshint node: true
 "use strict";
 
-var express = require('express');
+var express = require("express");
 var TutorialRouter = express.Router();
 
-const { userAuthorization } = require('../../helper/userAuthorization');
-const { upload } = require('../../helper/imageUpload');
+const { userAuthorization } = require("../../helper/userAuthorization");
+const { upload } = require("../../helper/imageUpload");
 
-TutorialRouter.route('/')
-  .post(userAuthorization, upload.any(), require('./postTutorial').postTutorial);
+TutorialRouter.route("/").post(
+  userAuthorization,
+  upload.any(),
+  require("./postTutorial").postTutorial
+);
 
-TutorialRouter.route('/:tutorialId')
-  .put(userAuthorization, upload.any(), require('./putTutorial').putTutorial);
+TutorialRouter.route("/:tutorialId").put(
+  userAuthorization,
+  upload.any(),
+  require("./putTutorial").putTutorial
+);
 
-TutorialRouter.route('/:tutorialId')
-  .delete(userAuthorization, require('./deleteTutorial').deleteTutorial);
+TutorialRouter.route("/:tutorialId").delete(
+  userAuthorization,
+  require("./deleteTutorial").deleteTutorial
+);
 
-TutorialRouter.route('/')
-  .get(require('./getTutorials').getTutorials);
+TutorialRouter.route("/").get(require("./getTutorials").getTutorials);
 
-TutorialRouter.route('/:tutorialId')
-  .get(require('./getTutorial').getTutorial);
+TutorialRouter.route("/getAllTutorials").get(
+  userAuthorization,
+  require("./getAllTutorials").getAllTutorials
+);
+
+TutorialRouter.route("/getUserTutorials").get(
+  userAuthorization,
+  require("./getUserTutorials").getUserTutorials
+);
+
+TutorialRouter.route("/:tutorialId").get(require("./getTutorial").getTutorial);
 
 module.exports = TutorialRouter;
